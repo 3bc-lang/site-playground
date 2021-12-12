5171495f9ca68044ec945f261dbb37cf180888b9
RodrigoDornelles <rodrigo@dornelles.me>
2021-12-12 16:59:58 -0300

    Playground frontend (#1)
    * feat: vue initial setup
    
    * feat: user must able to see basic informations about playground
    
    * feat: basic assets must be indexed on playground page
    
    * feat: app must works same in all browsers
    
    * feat: all app must to respect global rules styles
    
    * feat: user must see playground page with the correct styles
    
    * chore: developers mustn't commit useless or private files and folders

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script>
+import "./styles/reset.css"
+import "./styles/global.css"
+import "./styles/app.styles.css"
+
+</script>
+
+<template>
+  <section class="main-painel">
+    <section class="head-options">
+      <img class="head-options__logo" alt="3BC Lang logo" src="./assets/logo.png" />
+
+      <div class="head-options__options">
+        <button class="head-options__btn head-options__reset-button">Reset</button>
+        <button class="head-options__btn head-options__run-button">
+          <img src="./assets/play-stroked.svg" alt="play button">
+          <p>Run</p>
+        </button>
+      </div>
+    </section>
+
+    <section class="log">
+      <textarea class="log__console">print("Hello, world!")</textarea>
+      
+      <div class="log__input">
+        <input class="log__text" type="text">
+
+        <button class="log__send-button">
+          <img class="log__send-button-image" src="./assets/play-solid.svg" alt="play button">
+        </button>
+      </div>
+    </section>
+  </section>
+
+  <section class="secondary-painel">
+    <div class="secondary-painel__user-painel">
+      <div class="secondary-painel__options">
+        <button class="secondary-painel__option">examples</button>
+        <button class="secondary-painel__option">load file</button>
+        <button class="secondary-painel__option">disable file</button>
+      </div>
+
+      <textarea class="secondary-painel__file-view">print("Hello, world!")</textarea>
+    </div>
+
+    <textarea class="secondary-painel__error-log">print("Hello, world!")</textarea>
+  </section>
+  
+</template>
+
+<style>
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
